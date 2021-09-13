9f0102c0526869eb8293d0c99ef89ce82e9dec65
Miguel dos Santos Pereira <miguel.santos@zup.com.br>
2021-09-13 19:01:28 -0300

    termino de vdd dos testes do repository

--- src/__test__/adapter/out/AccountRepositoryImpl.test.ts
+++ src/__test__/adapter/out/AccountRepositoryImpl.test.ts
@@ -1,6 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
 import AccountRepositoryImpl from '../../../main/adapter/out/AccountRepositoryImpl';
 import { SingleTableModel } from '../../../main/adapter/out/dto/SingleTableModel';
 import Account, { ACCOUNT_ENTITY_NAME } from '../../../main/domain/Account';
+import AccountTransaction, {
+  ACCOUNT_TRANSACTION_ENTITY_NAME,
+} from '../../../main/domain/AccountTransaction';
+import TransactionType from '../../../main/domain/TransactionType';
 
 const TENANT = 'tenant';
 
@@ -78,12 +83,16 @@ describe('AccountRepositoryImpl', () => {
   describe('createAccount', () => {
     test('createAccount success', async () => {
       const repository = new AccountRepositoryImpl();
+      const putItemMethod = jest
+        .spyOn(AccountRepositoryImpl.prototype, 'putItem')
+        .mockResolvedValueOnce(undefined);
       const account = new Account(TENANT, '123', 0);
 
       const result = await repository.createAccount(account);
 
       expect(result).toBeDefined();
       expect(result).toBe(account);
+      expect(putItemMethod).toHaveBeenCalled();
     });
   });
 
@@ -119,6 +128,105 @@ describe('AccountRepositoryImpl', () => {
       await expect(repository.updateAccount(account)).rejects.toThrow(
         'nao foi possivel editar, conta inexistente. tenant=tenant , document=123',
       );
+    });
+  });
+
+  describe('createAccountTransaction', () => {
+    test('createAccountTransaction when have receiver only', async () => {
+      const repository = new AccountRepositoryImpl();
+      const putItemMethod = jest
+        .spyOn(AccountRepositoryImpl.prototype, 'putItem')
+        .mockResolvedValueOnce(undefined);
+
+      const accountTransaction = new AccountTransaction(
+        TENANT,
+        uuidv4(),
+        100,
+        TransactionType.DEPOSIT,
+        undefined,
+        '123',
+      );
+
+      const result = await repository.createAccountTransaction(
+        accountTransaction,
+      );
+
+      expect(result).toBeDefined();
+      expect(result).toBe(accountTransaction);
+      expect(putItemMethod).toHaveBeenCalledTimes(1);
+    });
+
+    test('createAccountTransaction when have receiver and payer', async () => {
+      const repository = new AccountRepositoryImpl();
+      const putItemMethod = jest
+        .spyOn(AccountRepositoryImpl.prototype, 'putItem')
+        .mockResolvedValueOnce(undefined)
+        .mockResolvedValueOnce(undefined);
+
+      const accountTransaction = new AccountTransaction(
+        TENANT,
+        uuidv4(),
+        100,
+        TransactionType.TRANSFER,
+        '321',
+        '123',
+      );
+
+      const result = await repository.createAccountTransaction(
+        accountTransaction,
+      );
+
+      expect(result).toBeDefined();
+      expect(result).toBe(accountTransaction);
+      expect(putItemMethod).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe('findAccountTransactions', () => {
+    test('findAccountTransactions when account doesnt have transactions', async () => {
+      const repository = new AccountRepositoryImpl();
+      jest
+        .spyOn(
+          AccountRepositoryImpl.prototype,
+          'getByPartitionKeyAndQuerySortKey',
+        )
+        .mockResolvedValueOnce([]);
+
+      const result = await repository.findAccountTransactions(TENANT, '123');
+
+      expect(result).toBeDefined();
+      expect(result.length).toBe(0);
+    });
+
+    test('findAccountTransactions when account have a transaction', async () => {
+      const repository = new AccountRepositoryImpl();
+
+      const fromDB = {
+        pk: `${TENANT}#${ACCOUNT_ENTITY_NAME}#123`,
+        sk: `${ACCOUNT_TRANSACTION_ENTITY_NAME}#123Transaction`,
+        amount: 100,
+        type: TransactionType.TRANSFER,
+        payer: '123',
+        receiver: '321',
+      } as SingleTableModel;
+
+      jest
+        .spyOn(
+          AccountRepositoryImpl.prototype,
+          'getByPartitionKeyAndQuerySortKey',
+        )
+        .mockResolvedValueOnce([fromDB]);
+
+      const result = await repository.findAccountTransactions(TENANT, '123');
+
+      expect(result).toBeDefined();
+      expect(result.length).toBe(1);
+      expect(result[0].tenant).toBe(TENANT);
+      expect(result[0].id).toBe('123Transaction');
+      expect(result[0].amount).toBe(100);
+      expect(result[0].type).toBe(TransactionType.TRANSFER);
+      expect(result[0].payer).toBe('123');
+      expect(result[0].receiver).toBe('321');
     });
   });
 });
